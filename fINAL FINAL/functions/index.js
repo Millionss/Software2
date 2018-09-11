fb4ddb601a4ecb85bae0a26e8142f16ecfd67e6a
Anton <atch97@gmail.com>
2018-09-11 17:45:37 -0500

    changes

--- fINAL FINAL/functions/index.js
+++ fINAL FINAL/functions/index.js
@@ -1,10 +1,13 @@
 const functions = require('firebase-functions');
 const express = require('express');
 
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
+
 const app = express();
 app.get('/timestamp', (request, response) => {
   response.send(`${Date.now()}`);
 });
-
 
 exports.app = functions.https.onRequest(app);
