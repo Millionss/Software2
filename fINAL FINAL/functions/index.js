08fb898a8133fee0b7860aedcf43ebf8e6ff064a
Anton <atch97@gmail.com>
2018-09-11 18:41:42 -0500

    avance con el login

--- fINAL FINAL/functions/index.js
+++ fINAL FINAL/functions/index.js
@@ -1,13 +1,17 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engine = require('pug')
 
 const firebaseApp = firebase.initializeApp(
   functions.config().firebase
 );
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.get('/login', (request, response) => {
+  response.render('login');
 });
 
 exports.app = functions.https.onRequest(app);
