b8b86ced1062e60b1f6dc01eeeed878d5d385c6d
Anton <atch97@gmail.com>
2018-09-11 20:57:16 -0500

    login done

--- fINAL FINAL/functions/index.js
+++ fINAL FINAL/functions/index.js
@@ -1,17 +1,38 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
+const firebase = require('firebase');
 const express = require('express');
 const engine = require('pug')
 
 const firebaseApp = firebase.initializeApp(
-  functions.config().firebase
+  {
+    apiKey: "AIzaSyC-o77UiDDryjw7blt2rmyk9y1X6JmzVMg",
+    authDomain: "software-2-b874c.firebaseapp.com",
+    databaseURL: "https://software-2-b874c.firebaseio.com",
+    projectId: "software-2-b874c",
+    storageBucket: "software-2-b874c.appspot.com",
+    messagingSenderId: "654921925436"
+  }
 );
+const auth = firebaseApp.auth()
 
 const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
+
 app.get('/login', (request, response) => {
   response.render('login');
+});
+
+app.post('/login_comprobar', (request, response) => {
+  const user = request.body.user
+  const pass = request.body.pass
+
+  auth.signInWithEmailAndPassword(user, pass).then((user) => {
+    return response.send("Login succesfull");
+  }).catch((err) => {
+    console.log(err.code);
+    return response.render('login');
+  })
 });
 
 exports.app = functions.https.onRequest(app);
