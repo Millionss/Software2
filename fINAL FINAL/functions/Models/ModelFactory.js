82a201b07c6dc6ce3fa641d9f65b072ade721b2e
Anton <atch97@gmail.com>
2018-09-25 12:18:12 -0500

    Update de Modelos

--- fINAL FINAL/functions/Models/ModelFactory.js
+++ fINAL FINAL/functions/Models/ModelFactory.js
@@ -1,26 +1,39 @@
 const Course = require('./Course.js');
-const User = require('./User.js');
+const users = require('./User.js');
 
 class ModelFactory {
+    /**
+     * Funcion que se encarga en crear un usario sin importar que tipo. 
+     * Retorna un usuario de clase Alumno, profesor o admin. En caso que sea ninguno devuelve undefined
+     * @param {Snapshot de usuario obtenido de Firebase} snapshot 
+     */
     createUser(snapshot) {
         const val = snapshot.val()
         var alumnee;
         switch (val.type) {
             case "alumnee":
-            alumnee = new User(snapshot.key, val.name, val.email, val.type, []);
+            alumnee = new users.Student(snapshot.key, val.name, val.email, val.type, []);
             break;
             case "professor":
-            alumnee = new User(snapshot.key, val.name, val.email, val.type);
+            alumnee = new users.Teacher(snapshot.key, val.name, val.email, val.type);
+            break;
+            case "admin":
+            //TODO
             break;
         }
         return alumnee;
     }
 
+    /**
+     * 
+     * @param {Snapshot de un curso obtenido de firebase} snapshotCourse 
+     * @param {Snapshot de un profesor obtenido de firebase} snapshotTeacher 
+     */
     createCourse(snapshotCourse, snapshotTeacher) {
         const val = snapshotCourse.val();
         var course;
         var teacher = this.createUser(snapshotTeacher)
-        course = new Course(snapshotCourse.key, val.name, val.section, teacher)
+        course = new Course(snapshotCourse.key, val.name, teacher)
         return course;
     }
 }
