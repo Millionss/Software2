abb2a95b1c2b0cd71f297f6bb2f9304e35d08aa7
Anton Tchistiakov <atch97@gmail.com>
2018-09-25 15:37:24 -0500

    Revert "Crear Asesoria (intento)  - Logica Admin"
    This reverts commit a0cb4ee5f9a9f3325414c18fa55af828400cecb1.

--- fINAL FINAL/functions/Models/Asesoria.js
+++
@@ -1,9 +0,0 @@
-class Asesoria{
-    constructor(id,teacher, courses) {
-        this.id = id
-        this.teacher = teacher
-        this.courses = courses
-    }
-}
-
-module.exports = Asesoria
--- fINAL FINAL/functions/Models/ModelFactory.js
+++ fINAL FINAL/functions/Models/ModelFactory.js
@@ -16,22 +16,7 @@ module.exports = {
             alumnee = new User(snapshot.key, val.name, val.email, val.type);
             break;
         }
-
+        
         return alumnee;
     }
-}
-
-module.exports = {
-    createAsesoria(snapshot) {
-        const val = snapshot.val()
-        var courses = []
-        var teacher
-        var asesoria;
-        console.log(val.type)
-        switch (val.type) {
-            asesoria= new Asesoria(snapshot.key, val.name, courses);
-            break;
-        }
-        return asesoria;
-    }
-}
+}    
