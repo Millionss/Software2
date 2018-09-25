eb0168526f19f6c76620d8bd0e8c936bf83f86c7
Anton <atch97@gmail.com>
2018-09-25 12:02:19 -0500

    Update User model

--- fINAL FINAL/functions/Models/User.js
+++ fINAL FINAL/functions/Models/User.js
@@ -1,11 +1,40 @@
 class User {
-    constructor(id, name, email, type, courses) {
+    constructor(id, name, email) {
         this.id = id
         this.name = name
         this.email = email
-        this.type = type
-        this.courses = courses
     }
 }
 
-module.exports = User
+class Student extends User {
+    constructor(id, name, email, courses) {
+        super(id, name, email)
+        self.courses = courses
+    }
+
+    filterCoursesByTeacher() {
+        var teachers = []
+        this.courses.forEach(course => {
+            var name = course.teacher
+            
+        })
+    }
+}
+
+class Teacher extends User {
+    constructor(id, name, email, courses) {
+        super(id, name, email)
+        self.courses = courses
+    }
+}
+
+class Admin extends User {
+
+}
+
+module.exports = {
+    User: User,
+    Studente: Student,
+    Teacher: Teacher,
+    Admin: Admin
+}
