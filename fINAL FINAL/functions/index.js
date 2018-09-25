898fbc71869e171c67c58a1e1734ff81c9a07045
Anton <atch97@gmail.com>
2018-09-25 17:33:33 -0500

    Ver asesorias en el main menu del profesor done

--- fINAL FINAL/functions/index.js
+++ fINAL FINAL/functions/index.js
@@ -5,16 +5,14 @@ const session = require('express-session');
 const engine = require('pug');
 const modelsClass = require('./Models/ModelFactory');
 
-const firebaseApp = firebase.initializeApp(
-  {
-    apiKey: "AIzaSyC-o77UiDDryjw7blt2rmyk9y1X6JmzVMg",
-    authDomain: "software-2-b874c.firebaseapp.com",
-    databaseURL: "https://software-2-b874c.firebaseio.com",
-    projectId: "software-2-b874c",
-    storageBucket: "software-2-b874c.appspot.com",
-    messagingSenderId: "654921925436"
-  }
-);
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyC-o77UiDDryjw7blt2rmyk9y1X6JmzVMg",
+  authDomain: "software-2-b874c.firebaseapp.com",
+  databaseURL: "https://software-2-b874c.firebaseio.com",
+  projectId: "software-2-b874c",
+  storageBucket: "software-2-b874c.appspot.com",
+  messagingSenderId: "654921925436"
+});
 const auth = firebaseApp.auth();
 const database = firebaseApp.database();
 const models = new modelsClass()
@@ -33,15 +31,15 @@ app.get('/', (request, response) => {
   if (auth.currentUser !== null) {
     const uid = auth.currentUser.uid
     if (typeof request.session.type !== "undefined") {
-      response.redirect(307, '/main_menu_'+request.session.type);
+      response.redirect(307, '/main_menu_' + request.session.type);
     } else {
-      database.ref('/users/'+uid+'/type').once('value').then( (snapshot) => {
+      database.ref('/users/' + uid + '/type').once('value').then((snapshot) => {
         request.session.type = snapshot.val()
-        return response.redirect('/main_menu_'+snapshot.val());
-      }).catch(err => console.log("Error: "+err));
+        return response.redirect('/main_menu_' + snapshot.val());
+      }).catch(err => console.log("Error: " + err));
     }
   } else {
-    response.redirect(307, '/login') 
+    response.redirect(307, '/login')
   }
 });
 
@@ -54,19 +52,19 @@ app.get('/main_menu_admin', (request, response) => {
 })
 
 app.get('/main_menu_alumnee', (request, response) => {
-  if (auth.currentUser !== null) { 
-    database.ref('/users/'+auth.currentUser.uid).once('value').then( snapshotUser => {
+  if (auth.currentUser !== null) {
+    database.ref('/users/' + auth.currentUser.uid).once('value').then(snapshotUser => {
       var coursesSnapshot = snapshotUser.child('courses')
       var student = models.createUser(snapshotUser)
-      console.log(student)
       var counter = 0
-      coursesSnapshot.forEach( course => {
+      coursesSnapshot.forEach(course => {
         counter++
-        database.ref('/courses/'+course.val()).once('value').then( snapshotCourse => {
+        database.ref('/courses/' + course.val()).once('value').then(snapshotCourse => {
           var teacherID = snapshotCourse.val().teacher;
-          database.ref('/users/'+teacherID).once('value').then( snapshotTeacher => {
+          database.ref('/users/' + teacherID).once('value').then(snapshotTeacher => {
             const course = models.createCourse(snapshotCourse, snapshotTeacher);
             student.courses.push(course)
+
             if (student.courses.length == counter) {
               const teachers = student.filterCoursesByTeacher()
               return response.render('main_menu_alumnee', {
@@ -74,10 +72,32 @@ app.get('/main_menu_alumnee', (request, response) => {
                 teachers: teachers
               });
             }
-          }).catch(err => console.log("Error: "+err))
-        }).catch(err => console.log("Error: "+err))
+          }).catch(err => console.log("Error: " + err))
+        }).catch(err => console.log("Error: " + err))
       })
-    }).catch(err => console.log("Error: "+err)); 
+    }).catch(err => console.log("Error: " + err));
+  } else {
+    response.redirect(307, '/login')
+  }
+})
+
+app.get('/main_menu_professor', (request, response) => {
+  if (auth.currentUser !== null) {
+    var teacher;
+    database.ref('/users/' + auth.currentUser.uid).once('value').then(snapshotUser => {
+      teacher = models.createUser(snapshotUser)
+      return database.ref('/consulting sessions').orderByChild('teacher').equalTo(teacher.id).once('value')
+    }).then(snapshotAsesorias => {
+      var asesorias = []
+      snapshotAsesorias.forEach(snap => {
+        var asesoria = models.createAsesoria(snap)
+        asesorias.push(asesoria)
+      })
+      return response.render('main_menu_professor', {
+        name: teacher.name,
+        asesorias: asesorias
+      })
+    })
   } else {
     response.redirect(307, '/login')
   }
@@ -89,7 +109,7 @@ app.post('/login_comprobar', (request, response) => {
   auth.signInWithEmailAndPassword(user, pass).then((user) => {
     return response.redirect(302, "/")
   }).catch((err) => {
-    console.log("Error "+err);
+    console.log("Error " + err);
     return response.render('login');
   })
 });
